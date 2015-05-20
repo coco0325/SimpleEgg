52e7f6c343bdf971afa0b87befc826405c051986
Brian <redpanda4552@gmail.com>
2015-05-20 00:24:37 -0400

    Add comment, unbreak plugin.yml

--- src/main/java/io/github/redpanda4552/SimpleEgg/SECommand.java
+++ src/main/java/io/github/redpanda4552/SimpleEgg/SECommand.java
@@ -2,6 +2,10 @@ package io.github.redpanda4552.SimpleEgg;
 
 import org.bukkit.command.CommandExecutor;
 
+/**
+ * This class exists only because otherwise there are issues with "plugin" being null
+ * and by some weirdness that I do not yet understand, this works.
+ */
 public abstract class SECommand implements CommandExecutor {
 
 	protected SimpleEggMain plugin;
