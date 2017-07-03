82649447d6ca466b2e90ebbba4a819e351addb0f
Brian <redpanda4552@gmail.com>
2017-07-02 22:23:01 -0400

    Change tag visibility with config setting

--- src/main/java/io/github/redpanda4552/SimpleEgg/util/Text.java
+++ src/main/java/io/github/redpanda4552/SimpleEgg/util/Text.java
@@ -25,9 +25,11 @@ package io.github.redpanda4552.SimpleEgg.util;
 
 import org.bukkit.ChatColor;
 
+import io.github.redpanda4552.SimpleEgg.Main;
+
 public class Text {
 
     public static final ChatColor a = ChatColor.AQUA;
     public static final ChatColor b = ChatColor.BLUE;
-    public static final String tag = a + "[SimpleEgg]" + b + " ";
+    public static final String tag = Main.getSelf().getConfig().getBoolean("show-tag") ? a + "[SimpleEgg]" + b + " " : b + "";
 }
